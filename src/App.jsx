225713fbca0d56117bb6a7b7cab929b37d68d22c
victesa <kirui6623@gmail.com>
2025-08-22 13:41:25 +0300

    Add GitHub Pages workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import NavBar from "./components/NavBar"
 import Home from './Home'
@@ -11,7 +11,7 @@ function App() {
        <Router>
         <NavBar/>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home/>} />
           <Route path="/works" element={<Works />} />
           <Route path='/ContactMe' element={<ContactMe/>}/>
           </Routes>
